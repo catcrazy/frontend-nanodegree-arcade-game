f9135c4e573db46141322ec4c27c182d51e15a70
catcrazy <nahoito2231@gmail.com>
2015-01-02 20:16:33 +0900

    Initial commit. The game is working with basic features.

--- js/app.js
+++ js/app.js
@@ -1,12 +1,21 @@
+goog.require('goog.array');
+
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y, v) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = x;
+    this.y = y;
+    this.v = v; //speed
 }
+
+Enemy.prototype.getRectangle = function() {
+    return [this.x, this.x + 101, this.y, this.y + 83];
+};
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -14,6 +23,12 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x =  this.x + dt * this.v;
+
+    // end of canvas
+    if (this.x > 505) {
+        goog.array.remove(allEnemies, this);
+    }
 }
 
 // Draw the enemy on the screen, required method for game
@@ -24,12 +39,90 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+/** @constructor */
+var Player = function(){
+    this.x = 200;
+    this.y = 400;
+    this.sprite = 'images/char-boy.png'
+};
 
+Player.prototype.getRectangle = function() {
+    return [this.x, this.x + 101, this.y, this.y + 83]
+};
+
+/**
+ * Check if it hits the enemies
+ * @param {string} foo This is a param with a description too long to fit in
+ *     one line.
+ * @return {number} This returns something that has a description too long to
+ *     fit in one line.
+ */
+Player.prototype.update = function() {
+    var isHit = false;
+    var player_rect = player.getRectangle();
+    var player_loc = [(player_rect[0] + player_rect[1]) / 2, (player_rect[2] + player_rect[3]) / 2];
+
+    allEnemies.forEach(function(en) {
+        var enemy_loc = en.getRectangle();
+        if (enemy_loc[0] < player_loc[0] && player_loc[0] < enemy_loc[1]
+            && enemy_loc[2] < player_loc[1] && player_loc[1] < enemy_loc[3]) {
+            console.log(enemy_loc);
+            isHit = true;
+        }
+    });
+
+    if (isHit) {
+        this.x = 200;
+        this.y = 400;
+    };
+
+    // if it goes out of canvas
+    if (this.x > 405 || this.x < -100 || this.y > 400 || this.y < -50) {
+    	this.x = 200;
+    	this.y = 400;
+    }
+
+};
+
+/**
+ * Illustrates line wrapping for long param/return descriptions.
+ * @param {string} foo This is a param with a description too long to fit in
+ *     one line.
+ * @return {number} This returns something that has a description too long to
+ *     fit in one line.
+ */
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(key) {
+    console.log(this.x, this.y);
+    if (!key)
+        return;
+    console.log(key);
+
+    var move_distance = {"up": [0, -83], "down": [0, 83], "left": [-101, 0], "right": [101, 0]};
+    this.x += move_distance[key][0];
+    this.y += move_distance[key][1];
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var allEnemies = [];
+var addEnemy = function() {
+    var init_loc = [[0, 50], [0, 50 +83], [0, 50 + 83*2]];
+    var rand_loc = init_loc[parseInt(Math.random() * 3)];
+    var rand_v = parseInt(Math.random() * 300 + 100);
 
+    var en = new Enemy(rand_loc[0], rand_loc[1], rand_v); 
+    allEnemies.push(en);
+    setTimeout(addEnemy, 1000);
+};
+
+addEnemy();
+
+var player = new Player();
 
 
 // This listens for key presses and sends the keys to your
